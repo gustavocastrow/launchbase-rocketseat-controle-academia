28ee2d361aefb2288915b45249a49890ef2b693c
Gustavo Castro <gustavocastrocs@gmail.com>
2019-12-05 08:26:52 -0300

    Apresentando os dados

--- instructors.js
+++ instructors.js
@@ -1,20 +1,39 @@
 const fs = require('fs')
 const data = require("./data.json")
 
+//show
+
+exports.show = function (req, res) {
+
+  const { id } = req.params
+
+  const foundInstructor = data.instructors.find(function(instructor){
+
+    return id == instructor.id 
+   
+
+  })
+
+  if (!foundInstructor) return res.send("Instructor not found")
+
+  const instructor = {
+    ...foundInstructor,
+    age: "",
+    gender: "",
+    services: "",
+    created_at: "",
+  }
+
+
+  return res.render("instructors/show", { instructor })
+
+}
+
 
 // create
 
 exports.post = function (req, res) {
-  //req.query
-  /*req.body
-  {
-    "avatar_url": "https://www.google.com",
-    "name": "Gustavo",
-    "birth": "1995-11-11",
-    "gender": "M",
-    "services": "Musculação,crossfit"
-  }
-  */
+
   const keys = Object.keys(req.body)
   //["avatar_url","name","birth","gender","services"]
 
@@ -25,15 +44,23 @@ exports.post = function (req, res) {
     }
   }
 
-  req.body.birth = Date.parse(req.body.birth)
-  req.body.created_at = Date.now()
+  let { avatar_url, birth, name, services, gender } = req.body
 
-  // [ ]
-  // [{...}] ...
-  //  [{...},  {...}] ...
+  birth = Date.parse(birth)
+  const created_at = Date.now()
+  const id = Number(data.instructors.length + 1)
 
-  data.instructors.push(req.body) // [{...}]
 
+
+  data.instructors.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at,
+  })
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
     if (err) return res.send("Write file error")
